3031d53d6bb2d0121919719b3bea4afa97495fe6
taste-the-fame <73516076+taste-the-fame@users.noreply.github.com>
2021-02-16 15:29:47 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const playlist = [
         wallet: 'DFiLmDMBdQT1546451FqMfcEsDmxDCFYxjD',
         handle: 'taste.the.fame',
         handleUrl: 'https://www.instagram.com/taste.the.fame/',
-        award: '<img src="https://media.giphy.com/media/9VcPcmoNpDulNJXSLw/giphy.gif">',
+        award: '',
         hasAward: false
     },
     {
@@ -15,32 +15,43 @@ const playlist = [
         title: 'Song 2',
         genre: 'Genre 2',
         songUrl: '<iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay"src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/293&amp;"></iframe>',
-        wallet: 'DFiLmDMBdQT1546451FqMfdfdxDCFYxjD'
+        wallet: 'DFiLmDMBdQT1546451FqMfdfdxDCFYxjD',
+        handle: 'taste.the.fame',
+        handleUrl: 'https://www.instagram.com/taste.the.fame/',
+        award: '<img src="https://media.giphy.com/media/9VcPcmoNpDulNJXSLw/giphy.gif">',
+        hasAward: true
     },
     {
         id: 3,
         title: 'Song 3',
         genre: 'Genre 3',
         songUrl: '<iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay"src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/293&amp;"></iframe>',
-        wallet: 'DFiLmDMBdQT1546451FqMfdfeegg4xjD'
+        wallet: 'DFiLmDMBdQT1546451FqMfdfeegg4xjD',
+        handle: 'taste.the.fame',
+        handleUrl: 'https://www.instagram.com/taste.the.fame/',
+        award: '',
+        hasAward: false
     }
 ];
 
 const sectionCenter = document.querySelector(".center_sect");
 const genreBtns = document.querySelectorAll(".filter_btn");
 
+setTimeout(function () {
+const awardImg = document.querySelector(".award");
+playlist.forEach(function (item) {
+    setTimeout(function(){}, 0)
+    if (item.hasAward === false) {
+        awardImg.classList.add('hidden');
+    } else {
+        console.log('false')
+    }
+  })
+}, 0);
+
 
 window.addEventListener("DOMContentLoaded", function () {
     displayListSongs(playlist);
-});
-window.addEventListener("DOMContentLoaded", function () {
-    const awardP = document.querySelector(".award");
-    if (playlist.hasAward === true) {
-       console.log('true')
-
-    } else {
-        awardP.classList.remove('awardGif');
-    }
 });
 
 genreBtns.forEach(function (btn) {
@@ -67,6 +78,7 @@ function displayListSongs(listSongs) {
         <div>${item.songUrl}</div>
         <div><a href="dogecoin:${item.wallet}">Donate</a></div>`;
     });
+
     displayList = displayList.join("");
     sectionCenter.innerHTML = displayList;
 };
